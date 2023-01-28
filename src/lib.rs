6839c199a67f1e1569fa9c4d529f72b973e0826f
dkhayes117 <dkhayes117@yahoo.com>
2023-01-28 14:37:32 -0500

    Update methods to async

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 #![no_std]
 #![feature(alloc_error_handler)]
 
+extern crate alloc;
+
+use alloc::format;
+use alloc::vec::Vec;
 use defmt_rtt as _; // global logger
 use panic_probe as _;
 use tinyrlibc as _;
@@ -12,15 +16,16 @@ pub mod keys;
 pub mod utils;
 
 use crate::config::{GOLIOTH_SERVER_PORT, GOLIOTH_SERVER_URL, SECURITY_TAG};
-use at_commands::parser::ParseError;
 use coap_lite::{error::MessageError, CoapRequest, ContentFormat, Packet, RequestType};
 use core::{
     str,
     sync::atomic::{AtomicU16, Ordering},
 };
 use defmt::info;
-use embassy_time::TimeoutError;
 use nrf_modem::{DtlsSocket, PeerVerification};
+use serde::de::DeserializeOwned;
+use serde::{ser, Serialize};
+
 // use serde::{de::DeserializeOwned, Serialize};
 
 /// Once flashed, comment this out along with the SPM entry in memory.x to eliminate flashing the SPM
@@ -35,8 +40,8 @@ pub enum Error {
     Coap(MessageError),
     Json(serde_json::error::Error),
     NrfModem(nrf_modem::Error),
-    Timeout(TimeoutError),
-    ParseError(ParseError),
+    Timeout(embassy_time::TimeoutError),
+    ParseError(at_commands::parser::ParseError),
 }
 
 impl From<MessageError> for Error {
@@ -57,16 +62,22 @@ impl From<nrf_modem::Error> for Error {
     }
 }
 
-impl From<TimeoutError> for Error {
-    fn from(e: TimeoutError) -> Self {
+impl From<embassy_time::TimeoutError> for Error {
+    fn from(e: embassy_time::TimeoutError) -> Self {
         Self::Timeout(e)
     }
 }
 
-impl From<ParseError> for Error {
-    fn from(e: ParseError) -> Self {
+impl From<at_commands::parser::ParseError> for Error {
+    fn from(e: at_commands::parser::ParseError) -> Self {
         Self::ParseError(e)
     }
+}
+
+// Enum for light_db write types
+pub enum LightDBType {
+    State,
+    Stream,
 }
 
 // static MESSAGE_ID_COUNTER: AtomicU16 = AtomicU16::new(0);
@@ -91,58 +102,76 @@ impl Golioth {
     }
 
     #[inline]
-    async fn make_request_and_recv(
-        &mut self,
-        data: &[u8],
-    ) -> Result<(), Error> {
+    async fn request_and_recv(&mut self, data: &[u8]) -> Result<heapless::Vec<u8,1024>, Error> {
         self.socket.send(data).await?;
 
         let mut buf = heapless::Vec::<u8, 1024>::new();
-        // unsafe { buf.set_len(1024) }
 
         let (response, _src_addr) = self.socket.receive_from(&mut buf[..]).await?;
 
         info!("{}", response);
+        let n = response.len();
+
+        buf.truncate(n);
+
+        Ok(buf)
+    }
+
+    async fn lightdb_read_raw(&mut self, path: &str) -> Result<Vec<u8>, Error> {
+        let mut request: CoapRequest<DtlsSocket> = CoapRequest::new();
+
+        // request.message.header.message_id = MESSAGE_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
+        request.set_method(RequestType::Get);
+        request.set_path(&format!(".d/{}", path));
+        request
+            .message
+            .set_content_format(ContentFormat::ApplicationJSON);
+
+        let resp = self.request_and_recv(&request.message.to_bytes()?).await?;
+
+        let packet = Packet::from_bytes(&resp)?;
+
+        Ok(packet.payload)
+    }
+
+    pub async fn lightdb_read_state<T: DeserializeOwned>(
+        &mut self,
+        path: &str,
+    ) -> Result<T, Error> {
+        let payload = self.lightdb_read_raw(path).await?;
+
+        Ok(serde_json::from_slice(&payload)?)
+    }
+
+    pub async fn lightdb_write<T: Serialize>(
+        &mut self,
+        db_type: LightDBType,
+        path: &str,
+        v: T,
+    ) -> Result<(), Error> {
+        let mut request: CoapRequest<DtlsSocket> = CoapRequest::new();
+
+        // request.message.header.message_id = MESSAGE_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
+        request.set_method(RequestType::Post);
+
+        let formatted_path = match db_type {
+            LightDBType::State => {
+                format!(".d/{}", path)
+            }
+            LightDBType::Stream => {
+                format!(".s/{}", path)
+            }
+        };
+
+        request.set_path(&formatted_path);
+
+        request
+            .message
+            .set_content_format(ContentFormat::ApplicationJSON);
+        request.message.payload = serde_json::to_vec(&v)?;
+
+        self.socket.send(&request.message.to_bytes()?).await?;
 
         Ok(())
     }
-
-    // fn lightdb_get_raw(&mut self, path: &str) -> Result<Vec<u8>, Error> {
-    //     let mut request: CoapRequest<()> = CoapRequest::new();
-    //
-    //     request.message.header.message_id = MESSAGE_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
-    //     request.set_method(RequestType::Get);
-    //     request.set_path(&format!(".d/{}", path));
-    //     request
-    //         .message
-    //         .set_content_format(ContentFormat::ApplicationJSON);
-    //
-    //     let resp = self.make_request_and_recv(&request.message.to_bytes()?)?;
-    //
-    //     let packet = Packet::from_bytes(&resp)?;
-    //
-    //     Ok(packet.payload)
-    // }
-    //
-    // pub fn lightdb_get<T: DeserializeOwned>(&mut self, path: &str) -> Result<T, Error> {
-    //     let payload = self.lightdb_get_raw(path)?;
-    //
-    //     Ok(serde_json::from_slice(&payload)?)
-    // }
-    //
-    // pub fn lightdb_set<T: Serialize>(&mut self, path: &str, v: T) -> Result<(), Error> {
-    //     let mut request: CoapRequest<()> = CoapRequest::new();
-    //
-    //     request.message.header.message_id = MESSAGE_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
-    //     request.set_method(RequestType::Post);
-    //     request.set_path(&format!(".d/{}", path));
-    //     request
-    //         .message
-    //         .set_content_format(ContentFormat::ApplicationJSON);
-    //     request.message.payload = serde_json::to_vec(&v)?;
-    //
-    //     self.socket.write(&request.message.to_bytes()?)?;
-    //
-    //     Ok(())
-    // }
 }
